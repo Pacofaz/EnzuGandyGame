058e8bf423e3435f4e30f5c52a11cac129036c5a
Edward 2 <bissingedward@gmail.com>
2025-06-21 14:35:07 +0200

    Bullets verändert

--- Zombie game/Edward/Bullet.cs
+++ Zombie game/Edward/Bullet.cs
@@ -1,5 +1,5 @@
-﻿
-using System.Drawing;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace ZombieGame.Entities
 {
@@ -26,7 +26,50 @@ namespace ZombieGame.Entities
 
         public override void Draw(Graphics g)
         {
-            g.FillEllipse(Brushes.White, Position.X, Position.Y, Size.Width, Size.Height);
+            var rect = new RectangleF(Position.X, Position.Y, Size.Width, Size.Height);
+
+            // 1) Großer, diffuser Glow
+            using (var glowPath = new GraphicsPath())
+            {
+                glowPath.AddEllipse(rect);
+                using (var glowBrush = new PathGradientBrush(glowPath))
+                {
+                    glowBrush.CenterColor = Color.FromArgb(0, 255, 50, 50);
+                    glowBrush.SurroundColors = new[] { Color.FromArgb(0, 255, 50, 50) };
+                    // Ausdehnung des Glows
+                    glowBrush.FocusScales = new PointF(0.8f, 0.8f);
+                    // Erweitere das Pfadgebiet für noch diffuseren Rand:
+                    var inflated = rect;
+                    inflated.Inflate(12, 12);
+                    using (var inflatedPath = new GraphicsPath())
+                    {
+                        inflatedPath.AddEllipse(inflated);
+                        g.FillPath(glowBrush, inflatedPath);
+                    }
+                }
+            }
+
+            // 2) Mittlerer Glow-Ring
+            using (var ringPath = new GraphicsPath())
+            {
+                // etwas kleinerer Ring
+                var ringRect = rect;
+                ringRect.Inflate(4, 4);
+                ringPath.AddEllipse(ringRect);
+                using (var ringBrush = new PathGradientBrush(ringPath))
+                {
+                    ringBrush.CenterColor = Color.FromArgb(120, 255, 50, 50);
+                    ringBrush.SurroundColors = new[] { Color.FromArgb(0, 255, 50, 50) };
+                    ringBrush.FocusScales = new PointF(0.5f, 0.5f);
+                    g.FillPath(ringBrush, ringPath);
+                }
+            }
+
+            // 3) Harter roter Kern
+            using (var coreBrush = new SolidBrush(Color.FromArgb(255, 255, 50, 50)))
+            {
+                g.FillEllipse(coreBrush, rect);
+            }
         }
 
         public bool IsOffMap(int mapWidth, int mapHeight)
