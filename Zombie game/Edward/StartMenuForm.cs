21ecf495ee02f6d1ad8bafc83bf8c556e013954a
Edward 2 <bissingedward@gmail.com>
2025-06-21 14:28:46 +0200

    Neue StartMenu

--- Zombie game/Edward/StartMenuForm.cs
+++ Zombie game/Edward/StartMenuForm.cs
@@ -1,35 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ZombieGame
 {
     public class StartMenuForm : Form
     {
-        // Ressourcen
         private Bitmap backgroundImage;
-        private List<Zombie> zombieList;
+        private Bitmap startButtonImage;
+        private Rectangle startButtonBounds;
+        private bool isHovering;
+
         private PrivateFontCollection fonts = new PrivateFontCollection();
-        private Font titleFont, subtitleFont, buttonFont, smallFont;
+        private Font titleFont;
 
-        // Menü
-        private string[] mainMenuItems = { "New Game", "Continue", "Settings", "Credits", "Exit" };
-        private Rectangle[] mainMenuItemBounds;
-        private int hoveredMainIndex = -1;
-        private bool inSettings, inCredits;
-
-        private Timer animationTimer;
+        // Button-Verschiebung in Pixel: 
+        // Positiv verschiebt nach rechts bzw. nach unten,
+        // negativ nach links bzw. nach oben.
+        private const int BUTTON_OFFSET_X = -10;
+        private const int BUTTON_OFFSET_Y = 460;
 
         public StartMenuForm()
         {
             InitializeForm();
             LoadAssets();
-            InitializeZombies();
-            CalculateMenuLayout();
-            SetupTimers();
+            PositionButton();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            PositionButton();
+            Invalidate();
         }
 
         private void InitializeForm()
@@ -38,76 +43,66 @@ namespace ZombieGame
             WindowState = FormWindowState.Maximized;
             TopMost = true;
             DoubleBuffered = true;
-            KeyDown += StartMenuForm_KeyDown;
-            MouseMove += StartMenuForm_MouseMove;
-            MouseClick += StartMenuForm_MouseClick;
+            KeyPreview = true;
+
+            MouseMove += OnMouseMove;
+            MouseClick += OnMouseClick;
+            KeyDown += OnKeyDown;
+            Resize += (s, e) => { PositionButton(); Invalidate(); };
         }
 
         private void LoadAssets()
         {
             string baseDir = Application.StartupPath;
-            backgroundImage = (Bitmap)Image.FromFile(System.IO.Path.Combine(baseDir, "assets", "bg_zombie_fullscreen.png"));
-            fonts.AddFontFile(System.IO.Path.Combine(baseDir, "assets", "ZombieApocalypse8bit.ttf"));
-            titleFont = new Font(fonts.Families[0], 50, FontStyle.Bold);
-            subtitleFont = new Font(fonts.Families[0], 48, FontStyle.Regular);
-            buttonFont = new Font(fonts.Families[0], 20, FontStyle.Regular);
-            smallFont = new Font(fonts.Families[0], 24, FontStyle.Regular);
+            backgroundImage = (Bitmap)Image.FromFile(Path.Combine(baseDir, "assets", "fullscreen.png"));
+            startButtonImage = (Bitmap)Image.FromFile(Path.Combine(baseDir, "assets", "start.png"));
+
+            fonts.AddFontFile(Path.Combine(baseDir, "assets", "ZombieApocalypse8bit.ttf"));
+            titleFont = new Font(fonts.Families[0], 60, FontStyle.Bold);
         }
 
-        private void InitializeZombies()
+        private void PositionButton()
         {
-            zombieList = new List<Zombie>();
-            var rnd = new Random();
-            int w = Screen.PrimaryScreen.Bounds.Width;
-            int h = Screen.PrimaryScreen.Bounds.Height;
-            for (int i = 0; i < 10; i++)
-                zombieList.Add(new Zombie
-                {
-                    Position = new PointF(rnd.Next(w), rnd.Next(h / 2, h)),
-                    Frame = 0,
-                    FrameCount = 4,
-                    FrameWidth = 64,
-                    FrameHeight = 64
-                });
-        }
+            var btnW = startButtonImage.Width;
+            var btnH = startButtonImage.Height;
 
-        private void CalculateMenuLayout()
-        {
-            int sw = Screen.PrimaryScreen.Bounds.Width;
-            int sh = Screen.PrimaryScreen.Bounds.Height;
-            int btnW = 300, btnH = 50, gap = 20;
-            int startY = (sh / 2) - ((mainMenuItems.Length * (btnH + gap)) / 2);
-            mainMenuItemBounds = new Rectangle[mainMenuItems.Length];
-            for (int i = 0; i < mainMenuItems.Length; i++)
-                mainMenuItemBounds[i] = new Rectangle((sw - btnW) / 2, startY + i * (btnH + gap), btnW, btnH);
-        }
+            // Center + Offset
+            var centerX = (ClientSize.Width - btnW) / 2;
+            var centerY = (ClientSize.Height - btnH) / 2;
 
-        private void SetupTimers()
-        {
-            animationTimer = new Timer { Interval = 200 };
-            animationTimer.Tick += (s, e) => {
-                // Animation der Zombies
-                for (int i = 0; i < zombieList.Count; i++)
-                {
-                    var z = zombieList[i];
-                    z.Frame = (z.Frame + 1) % z.FrameCount;
-                    zombieList[i] = z;
-                }
-                Invalidate();
-            };
-            animationTimer.Start();
+            var x = centerX + BUTTON_OFFSET_X;
+            var y = centerY + BUTTON_OFFSET_Y;
+
+            startButtonBounds = new Rectangle(x, y, btnW, btnH);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             var g = e.Graphics;
             SetRenderingHints(g);
-            g.DrawImage(backgroundImage, 0, 0, Width, Height);
-            DrawTitle(g);
-            if (inSettings) DrawSettingsMenu(g);
-            else if (inCredits) DrawCreditsScreen(g);
-            else DrawMainMenu(g);
 
+
+            g.DrawImage(backgroundImage, 0, 0, Width, Height);
+
+
+            if (isHovering)
+            {
+                var glowRect = startButtonBounds;
+                glowRect.Inflate(12, 12);
+                using (var path = new GraphicsPath())
+                {
+                    path.AddRectangle(glowRect);
+                    using (var brush = new PathGradientBrush(path))
+                    {
+                        brush.CenterColor = Color.FromArgb(180, Color.Yellow);
+                        brush.SurroundColors = new[] { Color.FromArgb(0, Color.Yellow) };
+                        g.FillPath(brush, path);
+                    }
+                }
+            }
+
+
+            g.DrawImage(startButtonImage, startButtonBounds);
         }
 
         private void SetRenderingHints(Graphics g)
@@ -119,114 +114,28 @@ namespace ZombieGame
             g.SmoothingMode = SmoothingMode.AntiAlias;
         }
 
-        private void DrawTitle(Graphics g)
+        private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            const string title = "ZOMBIE APOCALYPSE";
-            var ts = g.MeasureString(title, titleFont);
-            float x = (Width - ts.Width) / 2, y = 50;
-            g.DrawString(title, titleFont, Brushes.Black, x + 8, y + 8);
-            using (var lg = new LinearGradientBrush(new PointF(x, y), new PointF(x + ts.Width, y), Color.Red, Color.DarkRed))
-                g.DrawString(title, titleFont, lg, x, y);
-        }
-
-        private void DrawMainMenu(Graphics g)
-        {
-            for (int i = 0; i < mainMenuItems.Length; i++)
-                DrawButton(g, mainMenuItemBounds[i], mainMenuItems[i], hoveredMainIndex == i);
-        }
-
-        private void DrawSettingsMenu(Graphics g)
-        {
-            const string subtitle = "Settings";
-            var ss = g.MeasureString(subtitle, subtitleFont);
-            float sx = (Width - ss.Width) / 2, sy = 100;
-            g.DrawString(subtitle, subtitleFont, Brushes.White, sx, sy);
-        }
-
-        private void DrawCreditsScreen(Graphics g)
-        {
-            const string credits = "Developed by:\n- Team Alpha\n- Graphics by Beta Studios\n- Music by Gamma Beats";
-            g.DrawString(credits, smallFont, Brushes.LightGray, 100, 200);
-        }
-
-        private void DrawButton(Graphics g, Rectangle r, string text, bool hover)
-        {
-            using (var path = new GraphicsPath())
+            bool nowHover = startButtonBounds.Contains(e.Location);
+            if (nowHover != isHovering)
             {
-                path.AddRectangle(r);
-                using (var pgb = new PathGradientBrush(path))
-                {
-                    pgb.CenterColor = hover ? Color.DarkRed : Color.FromArgb(200, 50, 0, 0);
-                    pgb.SurroundColors = new[] { Color.Black };
-                    g.FillPath(pgb, path);
-                }
-            }
-            using (var pen = new Pen(Color.White, 4))
-                g.DrawRectangle(pen, r);
-            var ts = g.MeasureString(text, buttonFont);
-            float tx = (r.X + (r.Width - ts.Width) / 2), ty = (r.Y + (r.Height - ts.Height) / 2);
-            g.DrawString(text, buttonFont, Brushes.Black, tx + 2, ty + 2);
-            g.DrawString(text, buttonFont, hover ? Brushes.Yellow : Brushes.White, tx, ty);
-        }
-
-
-
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // StartMenuForm
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "StartMenuForm";
-            this.Load += new System.EventHandler(this.StartMenuForm_Load);
-            this.ResumeLayout(false);
-
-        }
-
-        private void StartMenuForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void StartMenuForm_MouseMove(object s, MouseEventArgs e)
-        {
-            if (!inSettings && !inCredits)
-            {
-                int prev = hoveredMainIndex;
-                hoveredMainIndex = Array.FindIndex(mainMenuItemBounds, r => r.Contains(e.Location));
-                if (prev != hoveredMainIndex) Invalidate();
+                isHovering = nowHover;
+                Invalidate(startButtonBounds);
             }
         }
 
-        private void StartMenuForm_MouseClick(object s, MouseEventArgs e)
+        private void OnMouseClick(object sender, MouseEventArgs e)
         {
-            if (!inSettings && !inCredits)
+            if (e.Button == MouseButtons.Left && startButtonBounds.Contains(e.Location))
             {
-                switch (hoveredMainIndex)
-                {
-                    case 0: DialogResult = DialogResult.OK; break;    // New Game
-                    case 1: DialogResult = DialogResult.OK; break;    // Continue
-                    case 2: inSettings = true; Invalidate(); break;
-                    case 3: inCredits = true; Invalidate(); break;
-                    case 4: DialogResult = DialogResult.Cancel; break;// Exit
-                }
+                DialogResult = DialogResult.OK;
             }
-            else { inSettings = inCredits = false; Invalidate(); }
         }
 
-        private void StartMenuForm_KeyDown(object s, KeyEventArgs e)
+        private void OnKeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Escape)
-            {
-                if (inSettings || inCredits) { inSettings = inCredits = false; Invalidate(); }
-                else DialogResult = DialogResult.Cancel;
-            }
-        }
-
-        private struct Zombie
-        {
-            public PointF Position; public int Frame, FrameCount, FrameWidth, FrameHeight;
+                DialogResult = DialogResult.Cancel;
         }
     }
 }
