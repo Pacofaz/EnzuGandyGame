2d286d41e3aea745e142d811773eda77ae1855f1
Selcuk58 <sel.demircan01@gmail.com>
2025-07-01 22:45:42 +0200

    Buttons für den Launcher eingebaut mit Bildern

--- EnzuLauncher/Code/EnzuLauncherV2/Form1.cs
+++ EnzuLauncher/Code/EnzuLauncherV2/Form1.cs
@@ -7,6 +7,11 @@ namespace EnzuLauncherV2
     public partial class FormMain : Form
     {
         private Image logoImage;
+        private Image candyImage;
+        private Image igorImage;
+
+        private Rectangle candyRect;
+        private Rectangle igorRect;
 
         public FormMain()
         {
@@ -20,25 +25,71 @@ namespace EnzuLauncherV2
             this.DoubleBuffered = true;
             this.BackColor = Color.FromArgb(43, 43, 43); // Dunkelgrau
 
-            // Logo laden 
+            // Bilder laden 
             logoImage = Image.FromFile("Resources/EnzuLogo.png");
+            candyImage = Image.FromFile("Resources/CandyGame.png");
+            igorImage = Image.FromFile("Resources/IgorSurvival.png");
+
+            // Bereiche für die Game-Bilder festlegen (Position und Größe)
+            int imageWidth = 320;
+            int imageHeight = 450;
+            int imageY = 180;
+            int padding = 120;
+            int leftX = (this.ClientSize.Width / 2) - imageWidth - (padding / 2);
+            int rightX = (this.ClientSize.Width / 2) + (padding / 2);
+
+            candyRect = new Rectangle(leftX, imageY, imageWidth, imageHeight);
+            igorRect = new Rectangle(rightX, imageY, imageWidth, imageHeight);
+
+            // MouseClick Event abonnieren
+            this.MouseClick += FormMain_MouseClick;
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            // Größe des Logos (z.B. 160x80 Pixel)
+            // Logo zeichnen
             int logoWidth = 160;
             int logoHeight = 140;
-
             int logoX = (this.ClientSize.Width - logoWidth) / 2;
-            int logoY = 5; // Abstand nach oben 
-
+            int logoY = 5;
             if (logoImage != null)
-            {
-                // Runterskaliert und mittig oben zeichnen
                 e.Graphics.DrawImage(logoImage, logoX, logoY, logoWidth, logoHeight);
+
+            // Candy Game Button mit Bild
+            if (candyImage != null)
+                e.Graphics.DrawImage(candyImage, candyRect);
+
+            // Igor Survival Button mit Bild
+            if (igorImage != null)
+                e.Graphics.DrawImage(igorImage, igorRect);
+
+            // Texte unterhalb der Bilder (Titel und Untertitel)
+            StringFormat centerFormat = new StringFormat { Alignment = StringAlignment.Center };
+
+            // Candy Game Text
+            var candyTitleY = candyRect.Bottom + 0;
+            var candySubtitleY = candyRect.Bottom + 40;
+            e.Graphics.DrawString("Candy Game", new Font("Segoe UI", 28, FontStyle.Regular), Brushes.White, candyRect.X + candyRect.Width / 2, candyTitleY, centerFormat);
+            e.Graphics.DrawString("Kostenlos", new Font("Segoe UI", 14, FontStyle.Regular), Brushes.LightGray, candyRect.X + candyRect.Width / 2, candySubtitleY, centerFormat);
+
+            // Igor Survival Text
+            var igorTitleY = igorRect.Bottom + 0;
+            var igorSubtitleY = igorRect.Bottom + 40;
+            e.Graphics.DrawString("Igor Survival", new Font("Segoe UI", 28, FontStyle.Regular), Brushes.White, igorRect.X + igorRect.Width / 2, igorTitleY, centerFormat);
+            e.Graphics.DrawString("Kostenlos", new Font("Segoe UI", 14, FontStyle.Regular), Brushes.LightGray, igorRect.X + igorRect.Width / 2, igorSubtitleY, centerFormat);
+        }
+
+        private void FormMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (candyRect.Contains(e.Location))
+            {
+                MessageBox.Show("Candy Game geklickt! (Hier kommt später das Preview-Fenster)");
+            }
+            else if (igorRect.Contains(e.Location))
+            {
+                MessageBox.Show("Igor Survival geklickt! (Hier kommt später das Preview-Fenster)");
             }
         }
     }
